d1a6842420d1ca07300c65a8b18a022d97fdd6ec
hypnosh <amit@13llama.com>
2014-05-12 22:52:20 +0530

    our base.js

--- appengine/js/base.js
+++ appengine/js/base.js
@@ -1,18 +1,25 @@
 /* base.js */
 $( function() {
-	$.ajax({
-		url: "/t/",
-		data: { contest_slug: 'slug', candidate_id: 'cid' }
-	}).done( function(data) {
-		console.log (data);
-		if (data.indexOf("200") > 1) {
-			//success, show alert
-			console.log('successfully updated');
-		} else {
-			// {'message': message }
 
-			var obj = JSON && JSON.parse(data) || $.parseJSON(data);
-			console.log(obj.message);
-		}
+	$(".vote").click( function() {
+		
+		// console.log('{ contest_slug:' + $("#contest_slug").val() + ', candidate_id:' + $(this).attr('data-candidate-id') + '}')
+		$.ajax({
+			type: 'POST',
+			url: "/t/",
+			data: { contest_slug: $("#contest_slug").val(), candidate_id: $(this).attr('data-candidate-id') }
+		}).done( function(data) {
+			console.log (data);
+			if (data.indexOf("200") > 1) {
+				//success, show alert
+				console.log('successfully updated');
+			} else {
+				// {'message': message }
+
+				var obj = JSON && JSON.parse(data) || $.parseJSON(data);
+				console.log(obj.message);
+			}
+		});
 	});
+
 });
